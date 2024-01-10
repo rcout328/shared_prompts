a34ef2aff8c2f4f2d684168a6cc941bd191d77a4
rcout328 <solovpxoffical@gmail.com>
2024-01-10 18:51:22 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "@/styles/globals.css"
+import Nav from '../components/Nav';
+import Provider from '../components/Provider';
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+
+const layout = ({children}) => {
+  return (
+    <html>
+      <body>
+      <Provider>
+      <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav/>
+          {children}
+        </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default layout
